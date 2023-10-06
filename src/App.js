c1ccd2d4f1111f70a850d55d31f787d2399d0e4e
Pradeep Kumar Gupta <120917855+Pradeep-Gupta-96@users.noreply.github.com>
2023-10-06 16:41:36 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+
 
 function App() {
+
+  const myAllroute = [
+    { path: "/", element: <HomePage/> },
+  ]
+  
   return (
-    <div className="App">
-     Jai shree Ganesh bhagwan ki
-    </div>
+    <>
+     <Router>
+        <Routes>
+          {myAllroute.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
