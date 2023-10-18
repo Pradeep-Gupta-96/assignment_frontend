e8f8ffbedeccbb1a544624504630bf19d3f1a01b
Areness Consulting <Pradeep-Gupta-96>
2023-10-18 18:03:14 +0530

    code updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../images/logo.png'
 import banner from '../images/main-banner.png'
+import videobanner from '../images/main-banner.mp4'
 import Grid from '@mui/material/Grid';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
@@ -37,7 +38,15 @@ const HomePage = () => {
             </div>
             <div className="banner">
                 <div className="bound">
-                    <div className="banner-box"><img src={banner} /></div>
+                    <div className="banner-box">
+                    <video
+                        autoPlay
+                        loop
+                        muted
+                    >
+                        <source src={videobanner} type="video/mp4" />
+                    </video>
+                    </div>
                 </div>
             </div>
             <div className="page-content">
@@ -52,46 +61,38 @@ const HomePage = () => {
                                 <div className="content-box">
                                     <div className="c-box-1">
                                         <div className="loction"><LocationOnOutlinedIcon />Gurugram</div>
+                                        <div className="time"><WorkOutlineIcon /> Full time</div>
                                         <div className="other-info">
-                                            <div className="time"><WorkOutlineIcon /> Full time</div>
                                             <div className="date"><AccessTimeIcon /> Posted 7 Days Ago</div>
                                             <div className="number"><ArticleIcon /> R0026172</div>
                                         </div>
                                     </div>
                                     <div className="c-box-2">
                                         <h3>Job Description:</h3>
-                                        <p>We're looking for an early stage career marketing professional to work with our team on all things marketing, including managing Purchase Orders and their payment, assisting with supplier support for all items related to conferences and events. To be on point for all new hire equipment and onboarding. To assist with meeting scheduling and support as determined by the team to be needed. The candidate will experience all areas of a well functioning marketing department including identity marketing - visual and verbal brand, digital marketing and market awareness. marketing for sales- lead generation and activities associated with communication and product marketing - the creation of message maps, asset matrix and a calendar of events.</p>
-                                        <h3>Responsibilities</h3>
+                                        <p>As an Assessment Intern at Areness, you will have the unique opportunity to gain invaluable experience in various areas of law. We are looking for a dedicated law fresher who is committed to providing exceptional legal research and eager to learn from experienced attorneys. </p>
+                                        <h3>Key Responsibilities:</h3>
+                                        <ol>
+                                            <li><strong>Legal Research:</strong> Conduct legal research to support senior attorneys and clients in various legal matters. Stay updated on relevant laws, regulations, and case precedents.</li>  
+                                            <li><strong>Documentation:</strong> Assist in drafting legal documents, contracts, and agreements under the guidance of senior attorneys. Ensure accuracy and compliance with legal standards.</li>  
+                                            <li><strong>Court Appearances:</strong> Attend court proceedings and hearings as required.</li>  
+                                            <li><strong>Case Analysis:</strong> Analyse legal issues and provide recommendations to senior attorneys. Contribute to the development of case strategies. </li>
+                                        </ol>
+                                        <h3>Qualifications:</h3>
+                                        <ol>
+                                            <li>Bachelor’s degree in Law (LLB or equivalent)</li> 
+                                            <li>Strong analytical and research skills.</li>
+                                            <li>Excellent written and verbal communication skills.</li> 
+                                            <li>Professionalism and commitment to maintaining client confidentiality.</li>
+                                        </ol>
+                                        <h3>Skills:</h3>
                                         <ul>
-                                            <li>Assist in editing content to post to the website, helping to identify content to be updated, and routing website inquiries to the appropriate teams</li>
-                                            <li>Perform market and industry research, competitive analyses, and business trend reporting</li>
-                                            <li>Prepare marketing activity and ROI reports by collecting and summarizing data to measure and assess program success</li>
-                                            <li>Support internal collaboration capabilities and intranet sites</li>
-                                            <li>Support virtual and in-person event planning, management, and follow-up</li>
-                                            <li>Assist in the implementation of the company’s brand campaigns</li>
-                                            <li>Support invoice submission and vendor onboarding processes</li>
-                                            <li>Expand knowledge and competencies through training and research</li>
-                                            <li>Support the Marketing practice with additional projects as needed.</li>
+                                            <li>Legal Knowledge</li>
+                                            <li>Research Skills</li> 
+                                            <li>Legal Writing</li> 
+                                            <li>Analytical Skills</li> 
+                                            <li>Negotiation</li> 
+                                            <li>Networking </li>
                                         </ul>
-                                        <h3>Requirements</h3>
-                                        <ul>
-                                            <li>Bachelor’s degree in marketing, business, or communications</li>
-                                            <li>Proficiency in Microsoft Office Suite: Word, PowerPoint, and Excel</li>
-                                            <li>Knowledge of traditional and digital marketing, business communications, and social media marketing</li>
-                                            <li>Excellent writing, communication, and presentation skills</li>
-                                            <li>Experience with research projects and business analytics</li>
-                                            <li>Highly organized with strong attention to detail and excellent project management skills</li>
-                                            <li>Ability to work remotely with a team, communicating effectively and managing time to meet deadlines</li>
-                                            <li>Solid work ethic, integrity, and reliability</li>
-                                        </ul>
-                                        <h3>Preferred Qualifications</h3>
-                                        <ul>
-                                            <li>Familiarity with content management systems (CMS) and customer relationship management (CRM) tools</li>
-                                            <li>Working knowledge of HTML and email software</li>
-                                            <li>Working knowledge of Adobe Creative Suite: InDesign, Photoshop, and Illustrator</li>
-                                        </ul>
-                                        <p>If you like wild growth and working with happy, enthusiastic over-achievers, you'll enjoy your career with us!</p>
-                                        <p>It is Epiq’s policy to comply with all applicable equal employment opportunity laws by making all employment decisions without unlawful regard or consideration of any individual’s race, religion, ethnicity, color, sex, sexual orientation, gender identity or expressions, transgender status, sexual and other reproductive health decisions, marital status, age, national origin, genetic information, ancestry, citizenship, physical or mental disability, veteran or family status or any other basis protected by applicable national, federal, state, provincial or local law. Epiq’s policy prohibits unlawful discrimination based on any of these impermissible bases, as well as any bases or grounds protected by applicable law in each jurisdiction. In addition Epiq will take affirmative action for minorities, women, covered veterans and individuals with disabilities. If you need assistance or an accommodation during the application process because of a disability, it is available upon request. Epiq is pleased to provide such assistance and no applicant will be penalized as a result of such a request.  Pursuant to relevant law, where applicable, Epiq will consider for employment qualified applicants with arrest and conviction records.</p>
                                     </div>
                                 </div>
                             </div>
